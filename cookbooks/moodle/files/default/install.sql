5fa9803373c4fa01bceb2d1627929053634d2339
Mark Nielsen <mark@moodlerooms.com>
2017-06-09 08:32:48 -0700

    Add comments to SQL file

--- cookbooks/moodle/files/default/install.sql
+++ cookbooks/moodle/files/default/install.sql
@@ -1,3 +1,5 @@
+-- This is supposed to roughly do the same thing as mysql_secure_installation except
+-- that it modifies the root user to have a password and allow connetion from any host.
 DELETE FROM mysql.user WHERE user = 'root' and host NOT IN ('127.0.0.1', 'localhost');
 DELETE FROM mysql.user WHERE USER LIKE '';
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
