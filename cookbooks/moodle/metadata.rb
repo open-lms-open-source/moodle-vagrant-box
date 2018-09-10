d40f611fe35877f31a599f0d4be77c177e6d1137
Guillermo Alvarez <guillermo.alvarez@blackboard.com>
2018-09-10 13:57:16 -0500

    Rebranding moodlerooms references

--- cookbooks/moodle/metadata.rb
+++ cookbooks/moodle/metadata.rb
@@ -1,6 +1,6 @@
 name             'moodle'
 maintainer       'Blackboard Inc'
-maintainer_email 'mark@moodlerooms.com'
+maintainer_email 'osdev@blackboard.com'
 license          'All rights reserved'
 description      'Configures a server environment for Moodle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
